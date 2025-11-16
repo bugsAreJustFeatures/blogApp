65d7841fe20c47a95631d332e030bbab999589cc
hxcrry <harryrobbo77@outlook.com>
2025-11-16 12:38:55 +0000

    add route that gets blogs for home page

---
+++ backend/routes/get_home_blogs.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, current_app, request, jsonify
+
+from database.database import get_home_blogs
+
+get_home_blogs_route_blueprint = Blueprint("home", __name__, url_prefix="/api")
+
+@get_home_blogs_route_blueprint.route("/home", methods=["GET"])
+def get_home_blogs_route():
+
+    # call db to get home blogs - this returns 10
+    home_blogs = get_home_blogs()
+    
+    return jsonify({
+        "error": False,
+        "message": "Blogs fetched successfully.",
+        "blogs": home_blogs
+    }), 200
