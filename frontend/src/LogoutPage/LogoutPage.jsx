117dc47b8b100014b2e049aee0297ae755ac59f4
hxcrry <harryrobbo77@outlook.com>
2025-11-18 14:24:43 +0000

    add logout functionality

--- frontend/src/LogoutPage/LogoutPage.jsx
+++ frontend/src/LogoutPage/LogoutPage.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom"
+
 import styles from "./LogoutPage.module.css"
 
 export default function LogoutPage() {
 
+    const navigate = useNavigate();
+
+    setTimeout(() => {
+        localStorage.removeItem("token")
+        navigate("/login")
+    }, 4000);
+
     return (
         <div className="universalWrapper">
-            
+            <h1>Logging you out and redirecting to the login page</h1>
         </div>
     )
 };
