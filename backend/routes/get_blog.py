6c6497f69710afad3aaf8db9fa0c1e921e46a51e
hxcrry <harryrobbo77@outlook.com>
2025-11-19 19:13:12 +0000

    improve json response from api and blueprint aspects

--- backend/routes/get_blog.py
+++ backend/routes/get_blog.py
@@ -4,12 +4,8 @@ from database.database import get_blog
 
 get_blog_blueprint = Blueprint("get", __name__, url_prefix="/api")
 
-@get_blog_blueprint.route("/get-blog", methods=["GET"])
-def get_blog():
-    data = request.get_json() # read request as json
-
-    blog_id = data["blog_id"] # get blog_id from request
-
+@get_blog_blueprint.route("/get-blog/<blog_id>", methods=["GET"])
+def get_blog_route(blog_id):
     blog = get_blog(blog_id) # get blog from db
 
     if not blog: # blog returned None so no blog could be found
@@ -19,18 +15,20 @@ def get_blog():
         }), 200
     
     # get blog details
-    blog_title = blog["title"]
-    blog_content = blog["content"]
-    blog_is_published = blog["is_published"]
-    blog_user_id = blog["user_id"]
+    blog_title = blog[0]
+    blog_content = blog[1]
+    blog_is_published = blog[2]
+    blog_created_on = blog[3]
+    blog_username = blog[4]
 
     return jsonify({
         "error": False,
         "message": "Found blog successfully.",
-        "blog_tile": blog_title,
-        "blog_content": blog_content,
-        "blog_is_published": blog_is_published,
-        "blog_user_id": blog_user_id
+        "title": blog_title,
+        "content": blog_content,
+        "is_published": blog_is_published,
+        "date": blog_created_on,
+        "username": blog_username
     }), 200
 
 
