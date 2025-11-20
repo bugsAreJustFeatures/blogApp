ec6e05c9de1a4517e5baf991d80be3213d807043
hxcrry <harryrobbo77@outlook.com>
2025-11-20 20:25:10 +0000

    add checks for whether user is searching for their own blogs, if so, fetch unpublsihed blogs as well

--- backend/routes/get_user_blogs.py
+++ backend/routes/get_user_blogs.py
@@ -1,17 +1,28 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, current_app, request, jsonify
 
-from database.database import get_user_blogs
+from database.database import get_user_blogs, get_user_blogs_unpublished
+
+from utils.is_self_search import is_self_search
 
 get_user_blogs_blueprint = Blueprint("get_user_blogs", __name__, url_prefix="/api")
 
 @get_user_blogs_blueprint.route("/get-user-blogs/<username>", methods=["GET"])
 def get_user_blogs_route(username):
 
-    # call db function to retrieve the blogs of this user
-    print(username)
-    blogs = get_user_blogs(username)
+    # check if the user is self searching
+    self_search = is_self_search(current_app, request, username)
+
+    if self_search:
+        # call db function to retrieve the blogs of this user
+        blogs = get_user_blogs_unpublished(username)
+    else:
+        # call db function to retrieve the blogs of another user
+        blogs = get_user_blogs(username)
+
+    # return to frontend
     return jsonify({
         "error": False,
         "message": f"Found blogs for {username}.",
+        "selfSearch": self_search,
         "blogs": blogs,
     }), 200
