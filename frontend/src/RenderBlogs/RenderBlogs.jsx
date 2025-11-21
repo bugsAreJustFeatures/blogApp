140d4b03f9e61d01ff784ea824dee7c34439b293
hxcrry <harryrobbo77@outlook.com>
2025-11-21 22:01:59 +0000

    made the layout better by including the wrapper inside of this component rather than the homePage and then the users page, also done css

--- frontend/src/RenderBlogs/RenderBlogs.jsx
+++ frontend/src/RenderBlogs/RenderBlogs.jsx
@@ -3,25 +3,47 @@ import styles from "./RenderBlogs.module.css"
 
 export default function renderBlogs(blogs) {
     const blogsArr = []; // create an array that will hold all the blogs
+    const blogsWrapper = []; // this will act as a wrapper for the blogs
     for (let i = 0; i < blogs.length; i++) { // loop through the blogs and get each of their parts
         let blogId = blogs[i][0]; // get the blog id
         let title = blogs[i][1];// get the blog title
-        let summary = blogs[i][2]; // get the blog content
-        let username = blogs[i][3];// get the username of the user who created the blog
-        let createdOn = blogs[i][4]; // get the creation time and date
+        let summary = blogs[i][2]; // get the blog summary
+        let createdOn = blogs[i][3];// get the username of the user who created the blog
+        let username = blogs[i][4]; // get the creation time and date
         // push the div as one so that each blog will have its own div
         blogsArr.push(
-        <div key={blogId} className={styles.blogContainer}>
-            <Link to={`/blogs/${blogId}`}>
-                <p>{blogId}</p>
-                <p>{title}</p>
-                <p>{summary}</p>
-                <p>{username}</p>
-                <p>{createdOn}</p>
-            </Link>
-            
-        </div>)
-    }
+            <div key={blogId} className={styles.individualBlogWrapper}>
+                <Link to={`/blogs/${blogId}`} >
+
+                    <div id={styles.titleWrapper}>
+                        <h1>Title: {title}</h1>
+                    </div>
+
+                    <div id={styles.summaryWrapper}>
+                        <h2>Summary: {summary}</h2>
+                    </div>
+
+                    <div id={styles.usernameWrapper}>
+                        <h3>Author: {username}</h3>
+                    </div>
+
+                    <div id={styles.createdOnWrapper}>
+                        <h4>Created On: {createdOn}</h4>
+                    </div>
+
+
+
+                </Link>
+                
+            </div>
+        )
+    };
+
+    blogsWrapper.push(
+        <div id={styles.blogsWrapper}>
+            {blogsArr}
+        </div>
+    );
     // return the newly made divs
-    return blogsArr;
+    return blogsWrapper;
 };
