449a13d14ce810b163ff9bc529b62a8372e33cbc
hxcrry <harryrobbo77@outlook.com>
2025-11-21 16:04:29 +0000

    made placeholder page for searching users

---
+++ frontend/src/SearchUser/SearchUser.jsx
@@ -0,0 +1,14 @@
+import styles from "./SearchUser.module.css";
+
+export default function SearchUser() {
+
+    return (
+        <>
+            <h1>For now, please search a user in the url such as</h1>
+
+            <h2>Remove the /search-user part, then do - /users/[username-goes-here]</h2>
+
+            
+        </>
+    )
+};
