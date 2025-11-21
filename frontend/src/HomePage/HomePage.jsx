09a3eb3a19f78c6d806cdf564d971d08c27c1bd5
hxcrry <harryrobbo77@outlook.com>
2025-11-21 14:42:28 +0000

    made DRY

--- frontend/src/HomePage/HomePage.jsx
+++ frontend/src/HomePage/HomePage.jsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 
 import styles from "./HomePage.module.css";
 
+import renderBlogs from "../RenderBlogs/RenderBlogs";
+
 export default function HomePage() {
 
     // state variables
     const [blogs, setBlogs] = useState(null); // used to store the blogs after fetching
+    const [blogsRendered, setBlogsRendered] = useState(null); // use to store the blogs after theyve been rendered
 
     const api = import.meta.env.VITE_API;
 
@@ -24,7 +27,9 @@ export default function HomePage() {
 
                 // update state with these newly fetched blogs
                 setBlogs(data.blogs);
-                console.log(data.blogs)
+                console.log(data.blogs);
+                const renderedBlogs = renderBlogs(data.blogs)
+                setBlogsRendered(renderedBlogs);
         
             } catch (err) {
                 throw new Error(err);
@@ -34,41 +39,20 @@ export default function HomePage() {
 
     }, [])
 
+    
+
     // checks if the blogs have been fetched otherwise just shows a "loading" message
-    if (!blogs) {
+    if (!blogs || !blogsRendered) {
         return <h1>Loading...</h1>
     };
 
-    function renderBlogs() {
-        const blogsArr = []; // create an array that will hold all the blogs
-        for (let i = 0; i < blogs.length; i++) { // loop through the blogs and get each of their parts
 
-            let blogId = blogs[i][0]; // get the blog id
-            let title = blogs[i][1];// get the blog title
-            let summary = blogs[i][2]; // get the blog content
-            let username = blogs[i][3];// get the username of the user who created the blog
-            let createdOn = blogs[i][4]; // get the creation time and date
-
-            // push the div as one so that each blog will have its own div
-            blogsArr.push(
-            <div key={blogId} className={styles.blogContainer}>
-                <p>{blogId}</p>
-                <p>{title}</p>
-                <p>{summary}</p>
-                <p>{username}</p>
-                <p>{createdOn}</p>
-            </div>)
-        }
-
-        // return the newly made divs
-        return blogsArr;
-    };
 
     return (
         <div className="universalWrapper">
             <div id={styles.homeBlogsWrapper}>
 
-                {renderBlogs()}
+                {blogsRendered}
             </div>
         </div>
     )
