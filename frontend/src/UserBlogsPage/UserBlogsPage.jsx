46c09bc80bf29cb61b5d405ecca9e62576507a44
hxcrry <harryrobbo77@outlook.com>
2025-11-21 14:42:36 +0000

    made DRY

--- frontend/src/UserBlogsPage/UserBlogsPage.jsx
+++ frontend/src/UserBlogsPage/UserBlogsPage.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react"
 import styles from "./UserBlogsPage.module.css"
 import { useParams } from "react-router-dom";
+import renderBlogs from "../RenderBlogs/RenderBlogs";
 
 export default function UserBlogsPage() {
 
     const [blogs, setBlogs] = useState(null);
+    const [blogsRendered, setBlogsRendered] = useState(null);
 
     const api = import.meta.env.VITE_API;
     const params = useParams()
@@ -24,44 +26,22 @@ export default function UserBlogsPage() {
             const data = await response.json()
             console.log(data)
             setBlogs(data.blogs)
+            const renderedBlogs = renderBlogs(data.blogs);
+            setBlogsRendered(renderedBlogs);
+
         };
         fetchUserBlogs();
 
     }, [])
-
-    function renderBlogs() {
-        const blogsArr = []; // create an array that will hold all the blogs
-        for (let i = 0; i < blogs.length; i++) { // loop through the blogs and get each of their parts
-
-            let blogId = blogs[i][0]; // get the blog id
-            let title = blogs[i][1];// get the blog title
-            let summary = blogs[i][2]; // get the blog content
-            let username = blogs[i][3];// get the username of the user who created the blog
-            let createdOn = blogs[i][4]; // get the creation time and date
-
-            // push the div as one so that each blog will have its own div
-            blogsArr.push(
-            <div key={blogId} className={styles.blogContainer}>
-                <p>{blogId}</p>
-                <p>{title}</p>
-                <p>{summary}</p>
-                <p>{username}</p>
-                <p>{createdOn}</p>
-            </div>)
-        }
-
-        // return the newly made divs
-        return blogsArr;
-    };
     
-    if (!blogs) {
+    if (!blogs || !blogsRendered) {
         return <h1>Loading...</h1>
     }
 
     return (
         <div className="universalWrapper">
             <div id={styles.userBlogsWrapper}>
-                {renderBlogs()}
+                {blogsRendered}
             </div>
         </div>
     )
