6c3275463aca3f6b2ffbed7a7784070cdc6e7f46
hxcrry <harryrobbo77@outlook.com>
2025-11-23 13:58:49 +0000

    add css and the form

--- frontend/src/SearchUser/SearchUser.jsx
+++ frontend/src/SearchUser/SearchUser.jsx
@@ -1,14 +1,30 @@
+import { useNavigate } from "react-router-dom";
+
 import styles from "./SearchUser.module.css";
 
 export default function SearchUser() {
 
+    // global component variables
+    const navigate = useNavigate();
+
+    function searchUser(e) {
+        e.preventDefault(); // prevent default form behaviour
+
+        const username = e.target.searchUsername.value;
+
+        navigate(`/users/${username}`);
+    };
+
     return (
-        <>
-            <h1>For now, please search a user in the url such as</h1>
+        <div className="universalWrapper">
+            <div id={styles.searchUserFormWrapper}>
+                <form onSubmit={(e) => {searchUser(e)}} id={styles.searchUserForm}>
+                    <label htmlFor="searchUsername">Search Username:</label>
+                    <input type="text" name="searchUsername" id={styles.searchUserInput} placeholder="user123" required />
 
-            <h2>Remove the /search-user part, then do - /users/[username-goes-here]</h2>
-
-            
-        </>
+                    <button type="submit">Search User</button>
+                </form>
+            </div>            
+        </div>
     )
 };
